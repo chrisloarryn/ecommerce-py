27b585a13f215a3fc386c7fdb26001bff0dd0801
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2021-04-05 18:06:48 -0400

    feat: cart screen functionality

--- frontend/src/screens/CartScreen/index.tsx
+++ frontend/src/screens/CartScreen/index.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, Card, Col, Form, Image, ListGroup, Row } from 'react-bootstrap'
 import { RouteComponentProps } from 'react-router-dom'
+import queryString from 'query-string'
+
+import Message from './../../components/Message'
+
+import { addToCart } from './../../actions/cartActions'
 
 import { TParams } from '../../types/globalTypes'
+import { GlobalReducersProductType } from '../../types/reducersTypes'
 
-const CartScreen: React.FC<RouteComponentProps<TParams>> = () => {
-  
+const CartScreen: React.FC<RouteComponentProps<TParams>> = ({
+  history,
+  match,
+  location
+}) => {
+  const dispatch = useDispatch()
+  const productId = match.params._id
+  const search = location.search ? queryString.parse(location.search).qty : '1'
+  const qtyNumber = Number(search)
+
+  const { cartItems } = useSelector(
+    (state: GlobalReducersProductType) => state.cart
+  )
+  console.log('cartItems', cartItems)
+
+  React.useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qtyNumber))
+    }
+  }, [dispatch, productId, qtyNumber])
   return (
     <div>
-      <h1>CartScreen</h1>
+      <h1>Cart Screen</h1>
     </div>
   )
 }
