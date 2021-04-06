56d3681a85033a6fa05a1162b85f6bfc90ee909f
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2021-04-06 00:20:44 -0400

    feat: cart items and shipping

--- frontend/src/screens/CartScreen/index.tsx
+++ frontend/src/screens/CartScreen/index.tsx
@@ -9,8 +9,9 @@ import Message from './../../components/Message'
 
 import { addToCart } from './../../actions/cartActions'
 
-import { TParams } from '../../types/globalTypes'
+import { ProductI, TParams } from '../../types/globalTypes'
 import { GlobalReducersProductType } from '../../types/reducersTypes'
+import { CartItemI } from '../../types/cartTypes'
 
 const CartScreen: React.FC<RouteComponentProps<TParams>> = ({
   history,
@@ -32,10 +33,117 @@ const CartScreen: React.FC<RouteComponentProps<TParams>> = ({
       dispatch(addToCart(productId, qtyNumber))
     }
   }, [dispatch, productId, qtyNumber])
+
+  const removeFromCartHandler = (_product: string) => {
+    console.log('====================================')
+    console.log(_product)
+    console.log('====================================')
+  }
+
+  type LocalType = ProductI & CartItemI
+
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
+  }
+
   return (
-    <div>
-      <h1>Cart Screen</h1>
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems?.length === 0 ? (
+          <Message variant='info'>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems?.map((item: CartItemI) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={3}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e: React.BaseSyntheticEvent) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[
+                        ...Array(
+                          (item as ProductI & CartItemI).countInStock
+                        ).keys()
+                      ].map((x: number) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={1}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal (
+                {cartItems?.reduce(
+                  (acc, item) => Number(acc) + Number(item.qty),
+                  0
+                )}
+                ) items
+              </h2>
+              $
+              {cartItems
+                ?.reduce(
+                  (acc, item) =>
+                    Number(acc) + Number(item.price) * Number(item.qty),
+                  0
+                )
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                style={{
+                  cursor: `${
+                    (cartItems as LocalType[]).length === 0
+                      ? 'not-allowed'
+                      : 'default'
+                  }`
+                }}
+                disabled={cartItems?.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
